4d859d7e27143d3c6ea18d679654e8d4ecdb0ef3
Mohamed Waqqas <mohamed.waqqas@gmail.com>
2022-07-12 18:55:30 +0300

    finished hello world exercise

---
+++ hello-world/src/lib.rs
@@ -0,0 +1,8 @@
+// &'static is a "lifetime specifier", something you'll learn more about later
+// pub fn hello() -> &'static str {
+//     "Goodbye, Mars!"
+// }
+
+pub fn hello() -> &'static str {
+    "Hello, World!"
+}
