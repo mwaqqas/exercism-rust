331bc5d16862b0411feac7ad051ee3de58025f9f
Mohamed Waqqas <mohamed.waqqas@gmail.com>
2022-08-01 15:00:50 +0300

    modified solution in accrodance with clippy's suggestions

--- bob/src/lib.rs
+++ bob/src/lib.rs
@@ -1,8 +1,8 @@
 pub fn reply(message: &str) -> &str {
     match message.trim() {
-        m if m.len() == 0 => "Fine. Be that way!",
-        m if is_question(m) && is_yelling(m) => "Calm down, I know what I'm doing!",
-        m if is_question(m) => "Sure.",
+        m if m.is_empty() => "Fine. Be that way!",
+        m if m.ends_with('?') && is_yelling(m) => "Calm down, I know what I'm doing!",
+        m if m.ends_with('?') => "Sure.",
         m if is_yelling(m) => "Whoa, chill out!",
         _ => "Whatever.",
     }
@@ -11,8 +11,4 @@ pub fn reply(message: &str) -> &str {
 fn is_yelling(message: &str) -> bool {
     let has_letters = message.chars().filter(|ch| ch.is_alphabetic()).count() > 0;
     has_letters && message.to_uppercase() == message
-}
-
-fn is_question(message: &str) -> bool {
-    message.trim().ends_with("?")
 }
