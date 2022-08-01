983fc81786c2a9e462e487b5a31e294ec41673c9
Mohamed Waqqas <mohamed.waqqas@gmail.com>
2022-08-01 14:05:44 +0300

    added functional solution

--- armstrong-numbers/src/lib.rs
+++ armstrong-numbers/src/lib.rs
@@ -1,26 +1,10 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    let digits = get_digits_of_num(num);
-
-    let mut sum = 0;
-    let len: u32 = digits.len() as u32;
-
-    for n in digits {
-        sum = sum + n.pow(len);
-    }
+    let sum: u32 = num
+        .to_string()
+        .chars()
+        .flat_map(|ch| ch.to_digit(10))
+        .map(|digit| digit.pow(num.to_string().len() as u32))
+        .sum::<u32>();
 
     sum == num
-}
-
-fn get_digits_of_num(num: u32) -> Vec<u32> {
-    let mut digits: Vec<u32> = Vec::new();
-
-    let mut n = num;
-    while n > 0 {
-        digits.push(n % 10);
-        n = n / 10;
-    }
-
-    digits.reverse();
-
-    digits
 }
