f448164e93fae77275fd39971cd8708be7b13bd1
Mohamed Waqqas <mohamed.waqqas@gmail.com>
2022-08-01 13:37:25 +0300

    added functional solution

--- difference-of-squares/src/lib.rs
+++ difference-of-squares/src/lib.rs
@@ -1,21 +1,9 @@
 pub fn square_of_sum(n: u32) -> u32 {
-    let mut sum = 0;
-
-    for i in 1..=n {
-        sum += i;
-    }
-
-    sum * sum
+    (1..n + 1).sum::<u32>().pow(2)
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    let mut sum = 0;
-
-    for i in 1..=n {
-        sum += i * i
-    }
-
-    sum
+    (1..n + 1).map(|x| x * x).sum()
 }
 
 pub fn difference(n: u32) -> u32 {
