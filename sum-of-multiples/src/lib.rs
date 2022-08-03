20090e8b8ef5b396a05c21a180356dbc7b8d5a47
Mohamed Waqqas <mohamed.waqqas@gmail.com>
2022-08-03 11:56:27 +0300

    added functional solution

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,21 +1,5 @@
-use std::collections::HashMap;
-
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    let mut multiples: HashMap<u32, bool> = HashMap::new();
-
-    for num in 1..limit {
-        for factor in factors {
-            if *factor != 0 && num % factor == 0 {
-                multiples.insert(num, true);
-            }
-        }
-    }
-
-    let mut sum: u32 = 0;
-
-    for entry in multiples {
-        sum += entry.0;
-    }
-
-    sum
+    (1..limit)
+        .filter(|num| factors.iter().any(|fac| *fac != 0 && num % fac == 0))
+        .sum()
 }
